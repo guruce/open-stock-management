b2456b1dfc41e5fc387445e21792512ac478221f
raajjya@gmail.com <raajjya@gmail.com@8b7b30bc-e56f-2154-180a-f5789eedddc6>
2012-08-25 10:28:05 +0000

    created modules for sales, client,transport,payment

---
+++ trunk/apps/frontend/modules/transport/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * transport actions.
+ *
+ * @package    open-stock-management
+ * @subpackage transport
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class transportActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
