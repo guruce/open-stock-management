2dbfdf9eb7d6c5c58f38e1b7e63e31585860455b
gurutharshan@gmail.com <gurutharshan@gmail.com@8b7b30bc-e56f-2154-180a-f5789eedddc6>
2012-08-25 12:35:19 +0000

    Add seperate folders for Dao, service, forms and exeption classes

---
+++ trunk/apps/frontend/lib/dao/ItemDao.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class ItemDao {
+    
+    /**
+     * Save Item
+     * 
+     * @param Item $item
+     * @throws DaoException 
+     */
+    public function saveItem(Item $item) {
+        try {
+            $item->save();
+        } catch (Exception $e) {
+            throw new DaoException($e->getMessage(), $e->getCode(), $e);
+        }        
+    }
+    
+    /**
+     * Get Item by id 
+     * 
+     * @param type $id
+     * @return type
+     * @throws DaoException 
+     */
+    public function getItemById($id) {
+        try {
+            return Doctrine_Core::getTable('Item')->find($id);
+        } catch (Exception $e) {
+            throw new DaoException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+    
+    public function updateItem(Item $item) {
+        try {
+            $query = Doctrine_Query::create()
+                        ->update('Item i');
+            $query->set('i.name', '?', $item->getName());
+            $query->set('i.sales_unit_price', '?', $item->getSalesUnitPrice());
+            $query->set('i.purchase_unit_price', '?', $item->getPurchaseUnitPrice());
+            $query->set('i.description', '?', $item->getDescription());
+            $query->set('i.stock_available', '?', $item->getStockAvailable());
+            $query->where('i.id = ?', $item->getId());
+            
+            return $query->execute();
+        } catch (Exception $e) {
+            throw new DaoException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+    
+    
+}
