83d3764fc16bad50daed476ca69271cf3fa69e90
gurutharshan@gmail.com <gurutharshan@gmail.com@8b7b30bc-e56f-2154-180a-f5789eedddc6>
2012-08-25 10:56:53 +0000

    addItem page added (action and success)

---
+++ apps/frontend/modules/default/actions/addItemAction.class.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of addItemAction
+ *
+ * @author guruce
+ */
+class addItemAction extends sfAction{
+    
+    /**
+     * Execute /../default/addItem
+     * 
+     * @param type $request 
+     */
+    public function execute($request) {
+        
+    }
+}
+
+?>
