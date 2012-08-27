27bff00ee5b587cff16d03521b65664fb66abe36
gurutharshan@gmail.com <gurutharshan@gmail.com@8b7b30bc-e56f-2154-180a-f5789eedddc6>
2012-08-27 03:10:56 +0000

    Add ItemSevice class and add some methods on ItemDao

---
+++ trunk/apps/frontend/lib/service/ItemService.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class ItemService {
+    
+    private $itemDao = null;
+    
+    public function __construct() {
+        $this->itemDao = new ItemDao();
+    }
+    
+    /**
+     * Return ItemDao class
+     * @return type 
+     */
+    public function getItemDao() {
+        return $this->itemDao;
+    }
+    
+    /**
+     * Set ItemDao object
+     * @param ItemDao $itemDao 
+     */
+    public function setItemDao(ItemDao $itemDao) {
+        $this->itemDao = $itemDao;
+    }
+    
+    /**
+     * Adding new Item 
+     * @param Item $item 
+     */
+    public function saveItem(Item $item) {
+        $this->itemDao->saveItem($item);
+    }
+    
+    /**
+     * Get Item by Id in service
+     * @param type $id
+     * @return type 
+     */
+    public function getItemById($id) {
+        return $this->itemDao->getItemById($id);
+    }
+    
+    /**
+     * Get all Items contents
+     * @return Item Doctrine-Collection 
+     */
+    public function getAllItems() {
+        return $this->itemDao->getAllItems();
+    }
+    
+    public function updateItem(Item $item) {
+        
+    }
+    
+}
+
+
