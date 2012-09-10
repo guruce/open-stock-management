65cfa14721508bcf6608b9835cf195a75f360894
raajjya@gmail.com <raajjya@gmail.com@8b7b30bc-e56f-2154-180a-f5789eedddc6>
2012-09-10 21:18:36 +0000

    completed leftSidebar component with dynamic menu and link arguments ,automatically css the active selected

--- apps/frontend/modules/default/templates/progressHomeSuccess.php
+++ apps/frontend/modules/default/templates/progressHomeSuccess.php
@@ -4,5 +4,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+include_component("commons", "leftSidebar",array("menu1"=>"myHome;default/progressHome" ,"menu2"=>"allItem;stock/blablabla","menu3"=>"allItem;stock/blablabla"));
 echo 'test ProgressHome by Mayureshan';
 ?>
