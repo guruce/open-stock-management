9f372f268e2c41d4cfe3a021f203184636e478f1
gurutharshan@gmail.com <gurutharshan@gmail.com@8b7b30bc-e56f-2154-180a-f5789eedddc6>
2012-09-27 16:33:31 +0000

    Rolled back to r39

--- apps/frontend/modules/contacts/actions/actions.class.php
+++ apps/frontend/modules/contacts/actions/actions.class.php
@@ -19,38 +19,4 @@ class contactsActions extends sfActions
   {
     //$this->forward('default', 'module');
   }
-  
-  public function executeShow(sfWebRequest $request)
-  {
-    $this->job = $this->getRoute()->getObject();
-    
-  }
-
-  public function executeNew(sfWebRequest $request)
-  {
-    $this->form = new JobeetJobForm();
-  }
-
-  public function executeCreate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST));
-
-    $this->form = new SellerForm();
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('newContact');
-  }
-
-
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $jobeet_job = $form->save();
-
-      $this->redirect('contacts/homeContacts');
-    }
-  }
 }
--- apps/frontend/modules/contacts/templates/homeContactsSuccess.php
+++ apps/frontend/modules/contacts/templates/homeContactsSuccess.php
@@ -4,7 +4,5 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-include_component("commons", "leftSidebar",array("menu1"=>"NewContact;contacts/newContact" ,"menu2"=>"ViewAll;contacts/viewAll","menu3"=>"Review;contacts/review"));
-
 echo 'Test HomeContacts';
 ?>
