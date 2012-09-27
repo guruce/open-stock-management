acc78db247384dd038a2b2f7cf6998b433a7b5e3
gurutharshan@gmail.com <gurutharshan@gmail.com@8b7b30bc-e56f-2154-180a-f5789eedddc6>
2012-09-27 16:33:31 +0000

    Rolled back to r39

--- trunk/apps/frontend/modules/contacts/templates/reviewSuccess.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-include_component("commons", "leftSidebar",array("menu1"=>"NewContact;contacts/newContact" ,"menu2"=>"ViewAll;contacts/viewAll","menu3"=>"Review;contacts/review"));
-echo "test review";
-?>
