3e7f6d88b1acd064f649154823f9bb7cb451d824
raajjya@gmail.com <raajjya@gmail.com@8b7b30bc-e56f-2154-180a-f5789eedddc6>
2013-01-18 02:02:54 +0000

    added deleteitem feature for stock module

--- apps/frontend/modules/stock/templates/showItemSuccess.php
+++ apps/frontend/modules/stock/templates/showItemSuccess.php
@@ -2,8 +2,9 @@
 include_component("commons", "leftSidebar", array("menu1" => "NewItem;stock/newItem", "menu2" => "AllItems;stock/listItem", "menu3" => "Review;stock/reviewItem"));
 use_stylesheet('stock/showItem.css')
 ?>
-<form action =<?php echo url_for('edit_item',$item)?>>
+
 <div id="showitem_contents">
+
     <div id="options"></div>
     <table>
 
@@ -20,14 +21,21 @@ use_stylesheet('stock/showItem.css')
         <tfoot>
             <tr>
                 <td colspan="2">
-                    <button type="submit" value="submit" >Edit this item..</button>
+                    <form action = "<?php echo url_for('delete_item', $item, array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>" method ="post">
+                        <input type="hidden" name="sf_method" value="delete" />
+                        <button type="submit" value="submit" >Remove this item..</button>
+                    </form>  
+
+                
+                    <form action = "<?php echo url_for('edit_item', $item) ?>">
+                        <button type="submit" value="submit" >Edit this item..</button>
+                    </form>  
+
                 </td>
             </tr>
         </tfoot>
-
-
-
     </table>
 
 </div>
-    </form>
+
+
